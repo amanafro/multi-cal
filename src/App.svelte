6293d20cd32ff29bc92e16f3798a186c29c4b75d
amanafro <amanuelisaak15@gmail.com>
2024-06-21 10:24:00 +0200

    dead

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,20 @@
 <script>
-  import Home from './pages/Home.svelte'
-  import Length from './pages/Length.svelte'
-  import Temperature from './pages/Temperature.svelte'
-  import Percentage from './pages/Percentage.svelte'
+  import Home from "./pages/Home.svelte";
+  import Length from "./pages/Length.svelte";
+  import Temperature from "./pages/Temperature.svelte";
+  import Percentage from "./pages/Percentage.svelte";
 
-  import Interest from './pages/Interest.svelte'
-  import Area from './pages/Area.svelte'
-  import { Router, Route } from 'svelte-routing';
+  import Interest from "./pages/Interest.svelte";
+  import Area from "./pages/Area.svelte";
+  import { Router, Route } from "svelte-routing";
 </script>
 
 <Router>
-    <Route path="/" component={Home}/>
-    <Route path="/Length" component={Length}/>
-    <Route path="/Temperature" component={Temperature}/>
-    <Route path="/Area" component={Area}/>
-    <Route path="/Percentage" component={Percentage}/>
-    <Route path="/Interest" component={Interest}/>
+  <Route path="/" component={Home} />
+  <Route path="/Length" component={Length} />
+  <Route path="/Temperature" component={Temperature} />
+  <Route path="/Area" component={Area} />
+  <Route path="/Percentage" component={Percentage} />
+  <Route path="/Interest" component={Interest} />
 </Router>
+
